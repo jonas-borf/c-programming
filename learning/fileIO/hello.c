e93f54e6ff726e35abac22ffe66d5169f658eb85
jonas-borf <jonasbarfield@gmail.com>
2025-09-24 10:15:06 -0400

    Wed Sep 24 10:15:06 AM EDT 2025

--- learning/fileIO/hello.c
+++ learning/fileIO/hello.c
@@ -5,14 +5,13 @@ int main(void)
     FILE *fp;
 
     int c;
-    
+
     fp = fopen("hello.txt", "r");
 
     while((c = fgetc(fp)) != EOF)
     {
         printf("%c", c);
     }
-    printf("\n");
 
     fclose(fp);
 }
