82544331fd3c97bf747ff3cca553594b4803ead1
jonas-borf <jonasbarfield@gmail.com>
2025-09-24 10:35:44 -0400

    Wed Sep 24 10:35:44 AM EDT 2025

--- learning/fileIO/formattedInput.c
+++ learning/fileIO/formattedInput.c
@@ -3,15 +3,15 @@
 int main(void)
 {
     FILE *fp;
-    char s[1024];
+    char name[1024];
     float length;
     int mass;
 
     fp = fopen("whales.txt", "r");
 
-    while(fscanf(fp, "%s $f $d", s, &length, &mass) != EOF)
+    while(fscanf(fp, "%s $f $d", name, &length, &mass) != EOF)
     {
-        printf("%s whale, %d tonnes, %.1f meters\n", s, mass, length);
+        printf("%s whale, %d tonnes, %.1f meters\n", name, mass, length);
     }
 
     fclose(fp);
