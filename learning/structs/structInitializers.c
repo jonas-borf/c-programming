4c386e47650b071b91c1ada3980d6fe03f7a6f79
jonas-borf <jonasbarfield@gmail.com>
2025-09-24 07:47:50 -0400

    Wed Sep 24 07:47:50 AM EDT 2025

--- learning/structs/structInitializers.c
+++ learning/structs/structInitializers.c
@@ -2,5 +2,19 @@
 
 int main(void)
 {
+    struct car {
+        char *name;
+        float price;
+        int speed;
+    };
+
+    struct car saturn = {"Saturn SL/2", 16000.99, 175};
+
+    
+    printf("Name:   %s\n", saturn.name);
+    printf("Price:  %f\n", saturn.price);
+    printf("Speed:  %d\n", saturn.speed);
+    
+    
 
 }
