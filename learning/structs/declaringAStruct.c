6b87802312725c928222697bc077d4cac18ab40d
jonas-borf <jonasbarfield@gmail.com>
2025-09-24 07:42:49 -0400

    Wed Sep 24 07:42:49 AM EDT 2025

--- learning/structs/declaringAStruct.c
+++ learning/structs/declaringAStruct.c
@@ -2,5 +2,19 @@
 
 int main(void)
 {
+    struct car {
+        char *name;
+        float price;
+        int speed;
+    };
 
+    struct car saturn;
+
+    saturn.name = "Saturn SL/2";
+    saturn.price = 15999.99;
+    saturn.speed = 175;
+
+    printf("Name:   %s\n", saturn.name);
+    printf("Price:  %f\n", saturn.price);
+    printf("Speed:  %d\n", saturn.speed);
 }
