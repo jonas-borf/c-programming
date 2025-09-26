c2ef1c1e23def2f4375f36518ff733b3f1242479
Jonas Barfield <jonasbarfield@gmail.com>
2025-09-26 10:01:54 -0400

    Fri Sep 26 10:01:54 AM EDT 2025

--- learning/pointersIIArithmetic/arrayPointerEquivalence/arrayPointerEquivalenceInFuncionCalls.c
+++ learning/pointersIIArithmetic/arrayPointerEquivalence/arrayPointerEquivalenceInFuncionCalls.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+int my_strlen(char *s)
+{
+  int count = 0;
+  char *t = s;
+  while(*t != '\0')
+  {
+    count++;
+    t++;
+  }
+
+  return count;
+}
+
 int main(void)
 {
+  char s[] = "Antelopes!";
+  char *t = "Wombats!";
+
+  printf("%d\n", my_strlen(t));
 
 }
