071c0b9e97ff13526a52a69b62b3844f8a20b4a8
Jonas Barfield <jonasbarfield@gmail.com>
2025-09-26 10:28:19 -0400

    Fri Sep 26 10:28:19 AM EDT 2025

--- learning/pointersIIArithmetic/my_memcpy.c
+++ learning/pointersIIArithmetic/my_memcpy.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+void *my_memcpy(void *dest, void *src, int byte_count)
+{
+  char *s = src, *d = dest;
+
+  while (byte_count--)
+  {
+    *d++ = *s++
+  }
+
+  return dest;
+}
+
 int main(void)
 {
+  char *s = "test";
+  char *t;
 
+  my_memcpy(t, s, 5);
+
+  printf("%s\n", t);
 }
