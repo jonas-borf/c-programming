61544afe5362bc7006e63a7f326b453c57c2eb2d
Jonas Barfield <jonasbarfield@gmail.com>
2025-09-27 12:18:56 -0400

    Sat Sep 27 12:18:56 PM EDT 2025

--- projects/erdos.c
+++ projects/erdos.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <math.h>
 
-long long factorial(int n)
+unsigned long long factorial(int n)
 {
-
-  if (n < 1)
-    return 0;
   if (n == 1)
     return 1;
 
@@ -16,12 +14,26 @@ int main(void)
 {
   int N = 70;
 
-  long long factorials[N];
+  unsigned long long factorials[N];
   
   for (int i = 0; i < 70; i++)
     factorials[i] = factorial(i + 1);
+  
+  for(int k = 3; k < 10; k++) {
+    for (int x = 1; x < 1000; x++) {
+      for (int y = x; y < 1000; y++) {
 
-  for (int i = 0; i < 70; i++) {
-    printf("%d\n", factorials[i]);
+        unsigned long long sum = pow(x, k) + pow(y, k);
+        unsigned long long diff = pow(y, k) - pow(x, k);
+        for (int i = 0; i < 70; i++) {
+          if (sum == factorials[i])
+            printf("sum (N, x, y, k) = (%d, %d, %d, %d)\n", i, x, y, k);
+          
+          if (diff == factorials[i])
+            printf("diff (N, x, y, k) = (%d, %d, %d, %d)\n", i, x, y, k);
+        }
+      }
+    }
   }
+
 }
