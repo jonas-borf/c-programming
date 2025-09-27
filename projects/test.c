ccb9941037d7f402eeffca9cb7204f4e3f2a2dc6
Jonas Barfield <jonasbarfield@gmail.com>
2025-09-27 17:13:40 -0400

    Sat Sep 27 05:13:40 PM EDT 2025

--- projects/test.c
+++ projects/test.c
@@ -3,10 +3,7 @@
 
 int main(void)
 {
-  double k = 5.0;
-  double z = 64.0 * log(2.0) / 5.0;
-  double *p;
-  p = &z;
-  double power = exp(*p);
+  double var = 10;
 
+  printf("%f\n", exp(var));
 }
