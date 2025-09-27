a178a28cdb62c7583148fe89cbd3fb571ee35bbf
Jonas Barfield <jonasbarfield@gmail.com>
2025-09-27 15:58:42 -0400

    Sat Sep 27 03:58:42 PM EDT 2025

--- virusBot.c
+++ virusBot.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 int main(void)
 {
+  printf("Creating VirusBot...\n Blowing up your computer in:");
 
+  for (int i = 5; i >= 0; i--)
+    printf("i\n");
+    sleep(1);
 }
